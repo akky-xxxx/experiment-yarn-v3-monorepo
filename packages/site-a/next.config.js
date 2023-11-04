de91e9d5f7a6a34aa1149799ddbb03d4f0c99953
akky-xxxx <114.s.aki@gmail.com>
2023-11-04 13:25:24 +0900

    chore(site-a): setup @swc-jotai/debug-label @swc-jotai/react-refresh

--- packages/site-a/next.config.js
+++ packages/site-a/next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  experimental: {
+    swcPlugins: [
+      ["@swc-jotai/debug-label", {}],
+      ["@swc-jotai/react-refresh", {}],
+    ],
+  },
 }
 
 module.exports = nextConfig
